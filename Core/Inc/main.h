fc977c738bfa5465ed3fc016de10936b3792a3e9
Edward Smith <ed18nsxr@gmail.com>
2021-12-29 17:27:04 -0500

    Added Ready_to_Drive() Function

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -68,6 +68,8 @@ void Error_Handler(void);
 #define PH1_OSC_OUT_GPIO_Port GPIOH
 #define OTG_FS_PowerSwitchOn_Pin GPIO_PIN_0
 #define OTG_FS_PowerSwitchOn_GPIO_Port GPIOC
+#define Start_Button_Pin GPIO_PIN_1
+#define Start_Button_GPIO_Port GPIOC
 #define PDM_OUT_Pin GPIO_PIN_3
 #define PDM_OUT_GPIO_Port GPIOC
 #define B1_Pin GPIO_PIN_0
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,10 +21,13 @@
 #include "main.h"
 #include "cmsis_os.h"
 #include "usb_host.h"
+#include "usb_host.h"
+
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -61,8 +64,11 @@ UART_HandleTypeDef huart2;
 osThreadId ledTaskHandle;
 osThreadId uartTaskHandle;
 /* USER CODE BEGIN PV */
-uint32_t appsVal[2];
-uint32_t bpsVal;
+uint32_t appsVal[2]; //to store APPS ADC values
+uint32_t apps_PP[2]; //to store APPS Pedal Position Values (in %)
+
+uint32_t bpsVal; //to store Brake Pressure Sensor value
+uint32_t bpsThreshold = 0; //need to set to store the signal value which corresponds to brakes being pressed
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -81,11 +87,26 @@ void startLEDTask(void const *argument);
 void startUART_Task(void const *argument);
 
 /* USER CODE BEGIN PFP */
-
+static bool Ready_to_Drive(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+CAN_TxHeaderTypeDef TxHeader;
+CAN_RxHeaderTypeDef RxHeader;
+
+uint8_t TxData[8];
+uint8_t RxData[8];
+
+uint32_t TxMailbox;
+
+void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+
+	if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK) {
+		Error_Handler();
+	}
+
+}
 
 /* USER CODE END 0 */
 
@@ -126,6 +147,31 @@ int main(void) {
 	MX_ADC3_Init();
 	MX_CAN1_Init();
 	/* USER CODE BEGIN 2 */
+	HAL_ADC_Start_DMA(&hadc1, &appsVal[0], 1); //start the ADC for APPS 1 in DMA mode
+	HAL_ADC_Start_DMA(&hadc3, &bpsVal, 1); //start the ADC for Brake Pressure Sensor in DMA mode
+	HAL_ADC_Start_DMA(&hadc2, &appsVal[1], 1); //start the ADC for APPS 2 in DMA mode
+
+	HAL_CAN_Start(&hcan1); //Start the CAN module
+
+	//Activate the CAN notification
+	if (HAL_CAN_ActivateNotification(&hcan1, CAN_IT_RX_FIFO0_MSG_PENDING)
+			!= HAL_OK) {
+		Error_Handler();
+	}
+
+	//Setting Required Data Values for CAN frame
+	TxHeader.DLC = 2;	//data length in bytes
+	TxHeader.IDE = CAN_ID_STD; //specify standard CAN ID
+	TxHeader.RTR = CAN_RTR_DATA;
+	TxHeader.StdId = 0x446;	//CAN ID of this device
+
+	TxData[0] = 50;
+	TxData[1] = 10;
+
+	//code not working starting here
+		if (HAL_CAN_AddTxMessage(&hcan1, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
+			Error_Handler();
+		}
 
 	/* USER CODE END 2 */
 
@@ -383,7 +429,19 @@ static void MX_CAN1_Init(void) {
 		Error_Handler();
 	}
 	/* USER CODE BEGIN CAN1_Init 2 */
+	CAN_FilterTypeDef canfilterconfig;
+	canfilterconfig.FilterActivation = CAN_FILTER_ENABLE;
+	canfilterconfig.FilterBank = 10;
+	canfilterconfig.FilterFIFOAssignment = CAN_FILTER_FIFO0;
+	canfilterconfig.FilterIdHigh = 0x446 << 5;
+	canfilterconfig.FilterIdLow = 0;
+	canfilterconfig.FilterMaskIdHigh = 0x446 << 5;
+	canfilterconfig.FilterMaskIdLow = 0x0000;
+	canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
+	canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
+	canfilterconfig.SlaveStartFilterBank = 20; // how many filters to assign to the CAN1 (master can)
 
+	HAL_CAN_ConfigFilter(&hcan1, &canfilterconfig);
 	/* USER CODE END CAN1_Init 2 */
 
 }
@@ -565,8 +623,7 @@ static void MX_GPIO_Init(void) {
 
 	/*Configure GPIO pin Output Level */
 	HAL_GPIO_WritePin(GPIOD,
-			LD4_Pin | LD3_Pin | LD5_Pin | LD6_Pin | Audio_RST_Pin,
-			GPIO_PIN_RESET);
+	LD4_Pin | LD3_Pin | LD5_Pin | LD6_Pin | Audio_RST_Pin, GPIO_PIN_RESET);
 
 	/*Configure GPIO pin : PE3 */
 	GPIO_InitStruct.Pin = GPIO_PIN_3;
@@ -582,11 +639,11 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(OTG_FS_PowerSwitchOn_GPIO_Port, &GPIO_InitStruct);
 
-	/*Configure GPIO pin : PC1 */
-	GPIO_InitStruct.Pin = GPIO_PIN_1;
+	/*Configure GPIO pin : Start_Button_Pin */
+	GPIO_InitStruct.Pin = Start_Button_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(Start_Button_GPIO_Port, &GPIO_InitStruct);
 
 	/*Configure GPIO pin : PDM_OUT_Pin */
 	GPIO_InitStruct.Pin = PDM_OUT_Pin;
@@ -639,7 +696,20 @@ static void MX_GPIO_Init(void) {
 }
 
 /* USER CODE BEGIN 4 */
+static bool Ready_to_Drive(void) {
 
+	for (;;) {
+		//checking if start button is pressed and brakes are pressed at the same time
+		if ((bpsVal >= bpsThreshold)
+				&& (!HAL_GPIO_ReadPin(GPIOC, Start_Button_Pin))) {
+			return true;
+		}
+
+		return false;
+
+	} //end for loop
+
+} //end Ready_to_Drive()
 /* USER CODE END 4 */
 
 /* USER CODE BEGIN Header_startLEDTask */
@@ -672,22 +742,21 @@ void startUART_Task(void const *argument) {
 	/* USER CODE BEGIN startUART_Task */
 	char startBtn[16] = "";
 	char msg[256];
-	uint32_t APPS1_PP;
-	uint32_t APPS2_PP;
+
 	/* Infinite loop */
 	for (;;) {
-		if (!HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_1)) {
+		if (!HAL_GPIO_ReadPin(GPIOC, Start_Button_Pin)) {
 			strcpy(startBtn, "Pressed");
 		} else {
 			strcpy(startBtn, "Not Pressed");
 		}
 
-		APPS1_PP = 0.0833 * appsVal[0];
-		APPS2_PP = -0.03339 * appsVal[1] + 136.7;
+		apps_PP[0] = 0.0833 * appsVal[0];
+		apps_PP[1] = -0.03339 * appsVal[1] + 136.7;
 
 		sprintf(msg,
 				"APPS_1 = %lu \t APPS_2 = %lu \t PP1 = %lu \t PP2 = %lu\r\n",
-				appsVal[0], appsVal[1], APPS1_PP, APPS2_PP);
+				appsVal[0], appsVal[1], apps_PP[0], apps_PP[1]);
 		HAL_UART_Transmit(&huart2, (uint8_t*) msg, strlen(msg), HAL_MAX_DELAY);
 		osDelay(100);
 	}
